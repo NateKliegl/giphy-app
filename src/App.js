47ddfa2d367141ee868693b00a0e34931053b61f
Nate Kliegl <nkliegl2@gmail.com>
2021-09-28 09:56:51 -0700

    implements menu component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Menu from "./components/Menu";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Menu />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Menu() {
+  return (
+    <nav className="menu">
+      <NavLink className="link" to="/login" activeClassName="active">
+        Login
+      </NavLink>
+      <NavLink className="link" to="/search" activeClassName="active">
+        Search
+      </NavLink>
+      <NavLink className="link" to="/favorites" activeClassName="active">
+        Favorties
+      </NavLink>
+    </nav>
+  );
+}
+export default Menu;
